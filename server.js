e8293e4bd77c3d58e56fd023b4dff7ff8c697047
info@santechlabs.com <info@santechlabs.com>
2021-06-01 21:46:04 +0530

    dfs

--- server.js
+++ server.js
@@ -12,20 +12,20 @@ app.use(express.json());
 
 
 
-const mongoose= require('mongoose');
-const uri= process.env.ATLAS_URI;
-console.log(uri);
-//mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true});
-mongoose.connect('mongodb+srv://Joicy:mean123@cluster0.rl35r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true});
+// const mongoose= require('mongoose');
+// const uri= process.env.ATLAS_URI;
+// console.log(uri);
+// //mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true});
+// mongoose.connect('mongodb+srv://Joicy:mean123@cluster0.rl35r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+// {useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true});
 
-const connection=mongoose.connection;
-connection.once('open',()=>{
-    console.log("MongoDB database connection establised successfully!!!");
-});
+// const connection=mongoose.connection;
+// connection.once('open',()=>{
+//     console.log("MongoDB database connection establised successfully!!!");
+// });
 
-const exercisesRouter=require('./routes/exercises');
-const usersRouter=require('./routes/users');
+// const exercisesRouter=require('./routes/exercises');
+// const usersRouter=require('./routes/users');
 
 app.use('/exercises',exercisesRouter);
 app.use('/users',usersRouter);
